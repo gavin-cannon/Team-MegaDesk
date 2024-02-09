de75a4b7802223e5efd671bc0bcc0db3e35dcf09
oliviaWil <olivia@tribewilson.com>
2024-02-09 13:10:17 -0600

    added dataviewgrid to viewallquotes

--- MegaDesk-Belnap/ViewAllQuotes.Designer.cs
+++ MegaDesk-Belnap/ViewAllQuotes.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.ViewAllQuotesCloseButton = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // ViewAllQuotesCloseButton
@@ -41,14 +45,38 @@
             this.ViewAllQuotesCloseButton.UseVisualStyleBackColor = true;
             this.ViewAllQuotesCloseButton.Click += new System.EventHandler(this.button1_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(83, 42);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(601, 305);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(487, 445);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(10, 10);
+            this.dataGridView2.TabIndex = 2;
+            // 
             // ViewAllQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.ViewAllQuotesCloseButton);
             this.Name = "ViewAllQuotes";
             this.Text = "View Quotes";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +84,7 @@
         #endregion
 
         private System.Windows.Forms.Button ViewAllQuotesCloseButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
--- MegaDesk-Belnap/ViewAllQuotes.cs
+++ MegaDesk-Belnap/ViewAllQuotes.cs
@@ -12,13 +12,19 @@ namespace MegaDesk_Belnap
 {
     public partial class ViewAllQuotes : Form
     {
+        public List<DeskQuote> deskQuotes = new List<DeskQuote>();
         public ViewAllQuotes(List<DeskQuote> quotes)
         {
+            deskQuotes = quotes;
             InitializeComponent();
+            
+            dataGridView1.DataSource = deskQuotes;
         }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
+            
             MainMenu viewMainMenu = (MainMenu)Tag;
             viewMainMenu.Show();
             Close();
