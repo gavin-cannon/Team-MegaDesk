92c4ea6a984e3c442d51b991661f7d8683100392
Elroe <elroe.tsigez@gmail.com>
2024-02-10 10:18:52 -0700

    Finished Validation for all except the Dropdown options.

--- MegaDesk-Belnap/AddQuote.Designer.cs
+++ MegaDesk-Belnap/AddQuote.Designer.cs
@@ -51,10 +51,10 @@
             // 
             // SubmitButton
             // 
-            this.SubmitButton.Location = new System.Drawing.Point(475, 307);
-            this.SubmitButton.Margin = new System.Windows.Forms.Padding(2);
+            this.SubmitButton.Location = new System.Drawing.Point(950, 590);
+            this.SubmitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.SubmitButton.Name = "SubmitButton";
-            this.SubmitButton.Size = new System.Drawing.Size(82, 37);
+            this.SubmitButton.Size = new System.Drawing.Size(164, 71);
             this.SubmitButton.TabIndex = 0;
             this.SubmitButton.Text = "Submit";
             this.SubmitButton.UseVisualStyleBackColor = true;
@@ -63,94 +63,98 @@
             // CustomerName
             // 
             this.CustomerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CustomerName.Location = new System.Drawing.Point(75, 61);
-            this.CustomerName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.CustomerName.Location = new System.Drawing.Point(150, 117);
+            this.CustomerName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.CustomerName.Name = "CustomerName";
-            this.CustomerName.Size = new System.Drawing.Size(130, 24);
+            this.CustomerName.Size = new System.Drawing.Size(260, 46);
             this.CustomerName.TabIndex = 1;
             this.CustomerName.Text = "Customer Name";
             // 
             // DeskWidth
             // 
             this.DeskWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DeskWidth.Location = new System.Drawing.Point(75, 93);
-            this.DeskWidth.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.DeskWidth.Location = new System.Drawing.Point(150, 179);
+            this.DeskWidth.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DeskWidth.Name = "DeskWidth";
-            this.DeskWidth.Size = new System.Drawing.Size(152, 24);
+            this.DeskWidth.Size = new System.Drawing.Size(304, 46);
             this.DeskWidth.TabIndex = 2;
             this.DeskWidth.Text = "Width (in inches)";
             // 
             // DeskDepth
             // 
             this.DeskDepth.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DeskDepth.Location = new System.Drawing.Point(75, 128);
-            this.DeskDepth.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.DeskDepth.Location = new System.Drawing.Point(150, 246);
+            this.DeskDepth.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DeskDepth.Name = "DeskDepth";
-            this.DeskDepth.Size = new System.Drawing.Size(130, 24);
+            this.DeskDepth.Size = new System.Drawing.Size(260, 46);
             this.DeskDepth.TabIndex = 3;
             this.DeskDepth.Text = "Depth (in inches)";
             // 
             // DesktopMaterialLabel
             // 
             this.DesktopMaterialLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DesktopMaterialLabel.Location = new System.Drawing.Point(75, 198);
-            this.DesktopMaterialLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.DesktopMaterialLabel.Location = new System.Drawing.Point(150, 381);
+            this.DesktopMaterialLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.DesktopMaterialLabel.Name = "DesktopMaterialLabel";
-            this.DesktopMaterialLabel.Size = new System.Drawing.Size(127, 26);
+            this.DesktopMaterialLabel.Size = new System.Drawing.Size(254, 50);
             this.DesktopMaterialLabel.TabIndex = 4;
             this.DesktopMaterialLabel.Text = "Desktop Material";
             // 
             // numOfDrawers
             // 
             this.numOfDrawers.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.numOfDrawers.Location = new System.Drawing.Point(75, 165);
-            this.numOfDrawers.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.numOfDrawers.Location = new System.Drawing.Point(150, 317);
+            this.numOfDrawers.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.numOfDrawers.Name = "numOfDrawers";
-            this.numOfDrawers.Size = new System.Drawing.Size(140, 26);
+            this.numOfDrawers.Size = new System.Drawing.Size(280, 50);
             this.numOfDrawers.TabIndex = 5;
             this.numOfDrawers.Text = "Number of Drawers";
             // 
             // RushOrder
             // 
             this.RushOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.RushOrder.Location = new System.Drawing.Point(75, 278);
-            this.RushOrder.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.RushOrder.Location = new System.Drawing.Point(150, 535);
+            this.RushOrder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.RushOrder.Name = "RushOrder";
-            this.RushOrder.Size = new System.Drawing.Size(140, 26);
+            this.RushOrder.Size = new System.Drawing.Size(280, 50);
             this.RushOrder.TabIndex = 6;
             this.RushOrder.Text = "Rush Order";
             // 
             // CustomerInputBox
             // 
-            this.CustomerInputBox.Location = new System.Drawing.Point(252, 63);
-            this.CustomerInputBox.Margin = new System.Windows.Forms.Padding(2);
+            this.CustomerInputBox.Location = new System.Drawing.Point(504, 121);
+            this.CustomerInputBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CustomerInputBox.Name = "CustomerInputBox";
-            this.CustomerInputBox.Size = new System.Drawing.Size(172, 20);
+            this.CustomerInputBox.Size = new System.Drawing.Size(340, 31);
             this.CustomerInputBox.TabIndex = 7;
+            this.CustomerInputBox.TextChanged += new System.EventHandler(this.CustomerInputBox_TextChanged);
+            this.CustomerInputBox.Validating += new System.ComponentModel.CancelEventHandler(this.customerInputBox_Validating);
             // 
             // DeskWidthInputBox
             // 
-            this.DeskWidthInputBox.Location = new System.Drawing.Point(252, 95);
-            this.DeskWidthInputBox.Margin = new System.Windows.Forms.Padding(2);
+            this.DeskWidthInputBox.Location = new System.Drawing.Point(504, 183);
+            this.DeskWidthInputBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DeskWidthInputBox.Name = "DeskWidthInputBox";
-            this.DeskWidthInputBox.Size = new System.Drawing.Size(76, 20);
+            this.DeskWidthInputBox.Size = new System.Drawing.Size(148, 31);
             this.DeskWidthInputBox.TabIndex = 8;
+            this.DeskWidthInputBox.Validating += new System.ComponentModel.CancelEventHandler(this.deskWidthInputBox_Validating);
             // 
             // DeskDepthInputBox
             // 
-            this.DeskDepthInputBox.Location = new System.Drawing.Point(252, 129);
-            this.DeskDepthInputBox.Margin = new System.Windows.Forms.Padding(2);
+            this.DeskDepthInputBox.Location = new System.Drawing.Point(504, 248);
+            this.DeskDepthInputBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DeskDepthInputBox.Name = "DeskDepthInputBox";
-            this.DeskDepthInputBox.Size = new System.Drawing.Size(76, 20);
+            this.DeskDepthInputBox.Size = new System.Drawing.Size(148, 31);
             this.DeskDepthInputBox.TabIndex = 9;
+            this.DeskDepthInputBox.Validating += new System.ComponentModel.CancelEventHandler(this.deskDepthInputBox_Validating);
             // 
             // threeDayRush
             // 
             this.threeDayRush.AutoSize = true;
-            this.threeDayRush.Location = new System.Drawing.Point(252, 278);
-            this.threeDayRush.Margin = new System.Windows.Forms.Padding(2);
+            this.threeDayRush.Location = new System.Drawing.Point(504, 535);
+            this.threeDayRush.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.threeDayRush.Name = "threeDayRush";
-            this.threeDayRush.Size = new System.Drawing.Size(81, 17);
+            this.threeDayRush.Size = new System.Drawing.Size(155, 29);
             this.threeDayRush.TabIndex = 10;
             this.threeDayRush.TabStop = true;
             this.threeDayRush.Text = "3 Day Rush";
@@ -159,10 +163,10 @@
             // fiveDayRush
             // 
             this.fiveDayRush.AutoSize = true;
-            this.fiveDayRush.Location = new System.Drawing.Point(252, 299);
-            this.fiveDayRush.Margin = new System.Windows.Forms.Padding(2);
+            this.fiveDayRush.Location = new System.Drawing.Point(504, 575);
+            this.fiveDayRush.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.fiveDayRush.Name = "fiveDayRush";
-            this.fiveDayRush.Size = new System.Drawing.Size(81, 17);
+            this.fiveDayRush.Size = new System.Drawing.Size(155, 29);
             this.fiveDayRush.TabIndex = 11;
             this.fiveDayRush.TabStop = true;
             this.fiveDayRush.Text = "5 Day Rush";
@@ -171,10 +175,10 @@
             // sevenDayRush
             // 
             this.sevenDayRush.AutoSize = true;
-            this.sevenDayRush.Location = new System.Drawing.Point(252, 320);
-            this.sevenDayRush.Margin = new System.Windows.Forms.Padding(2);
+            this.sevenDayRush.Location = new System.Drawing.Point(504, 615);
+            this.sevenDayRush.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.sevenDayRush.Name = "sevenDayRush";
-            this.sevenDayRush.Size = new System.Drawing.Size(81, 17);
+            this.sevenDayRush.Size = new System.Drawing.Size(155, 29);
             this.sevenDayRush.TabIndex = 12;
             this.sevenDayRush.TabStop = true;
             this.sevenDayRush.Text = "7 Day Rush";
@@ -183,10 +187,10 @@
             // noRush
             // 
             this.noRush.AutoSize = true;
-            this.noRush.Location = new System.Drawing.Point(252, 341);
-            this.noRush.Margin = new System.Windows.Forms.Padding(2);
+            this.noRush.Location = new System.Drawing.Point(504, 656);
+            this.noRush.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.noRush.Name = "noRush";
-            this.noRush.Size = new System.Drawing.Size(145, 17);
+            this.noRush.Size = new System.Drawing.Size(286, 29);
             this.noRush.TabIndex = 13;
             this.noRush.TabStop = true;
             this.noRush.Text = "None (Standard 14 Days)";
@@ -194,19 +198,20 @@
             // 
             // DrawersInput
             // 
-            this.DrawersInput.Location = new System.Drawing.Point(252, 167);
-            this.DrawersInput.Margin = new System.Windows.Forms.Padding(2);
+            this.DrawersInput.Location = new System.Drawing.Point(504, 321);
+            this.DrawersInput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.DrawersInput.Name = "DrawersInput";
-            this.DrawersInput.Size = new System.Drawing.Size(90, 20);
+            this.DrawersInput.Size = new System.Drawing.Size(180, 31);
             this.DrawersInput.TabIndex = 15;
+            this.DrawersInput.Validating += new System.ComponentModel.CancelEventHandler(this.drawersInput_Validating);
             // 
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(76, 17);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(152, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(266, 28);
+            this.label1.Size = new System.Drawing.Size(532, 54);
             this.label1.TabIndex = 16;
             this.label1.Text = "All fields must be completed:";
             // 
@@ -219,9 +224,10 @@
             "Pine",
             "Rosewood",
             "Venner"});
-            this.material.Location = new System.Drawing.Point(252, 198);
+            this.material.Location = new System.Drawing.Point(504, 381);
+            this.material.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.material.Name = "material";
-            this.material.Size = new System.Drawing.Size(121, 21);
+            this.material.Size = new System.Drawing.Size(238, 33);
             this.material.TabIndex = 17;
             // 
             // rushDays
@@ -232,16 +238,17 @@
             "3",
             "5",
             "7"});
-            this.rushDays.Location = new System.Drawing.Point(78, 307);
+            this.rushDays.Location = new System.Drawing.Point(156, 590);
+            this.rushDays.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.rushDays.Name = "rushDays";
-            this.rushDays.Size = new System.Drawing.Size(121, 21);
+            this.rushDays.Size = new System.Drawing.Size(238, 33);
             this.rushDays.TabIndex = 18;
             // 
             // AddQuotes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(621, 392);
+            this.ClientSize = new System.Drawing.Size(1242, 754);
             this.Controls.Add(this.rushDays);
             this.Controls.Add(this.material);
             this.Controls.Add(this.label1);
@@ -260,7 +267,7 @@
             this.Controls.Add(this.DeskWidth);
             this.Controls.Add(this.CustomerName);
             this.Controls.Add(this.SubmitButton);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AddQuotes";
             this.Text = "Add Quotes";
             ((System.ComponentModel.ISupportInitialize)(this.DrawersInput)).EndInit();
